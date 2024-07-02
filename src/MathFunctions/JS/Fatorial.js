429b280e55033ae55b6ffb3bbaeefbf671e7807a
ProdukerBy911 <bnwcrypee@gmail.com>
2024-07-02 19:00:04 -0300

    delta

--- src/MathFunctions/JS/Fatorial.js
+++ src/MathFunctions/JS/Fatorial.js
@@ -1,11 +1,12 @@
 const NumFact = 120;
-const Result = 
+const Result = NumFact * (NumFact - 1) * (NumFact -2); 
 
 function fatorial(NumFact) {
   if (NumFact == 0) {
     return print("Defina um valor inteiro maior que 10 para calcular o fatorial:  ");
   } else {
-    Result = NumFact * (NumFact - 1) * (NumFact -2);
+    return Result; 
   }
-  console.log(Result);
 }
+
+console.log(fatorial);
