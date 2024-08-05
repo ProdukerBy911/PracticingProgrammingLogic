d2acb2c28974a80868a5a5f97c3ce2ff4033a456
ProdukerBy911 <bnwcrypee@gmail.com>
2024-08-05 15:08:55 -0300

    upload projects to Alura to pratice program logic

---
+++ 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -0,0 +1,9 @@
+function sortear() {
+    let quantidade = document.getElementById('quantidade').value;
+    let ate = document.getElementById('ate').value;
+    let de = document.getElementById('de').value;
+
+    alert('quantidade, ${quantidade}');
+    alert('ate, ${ate}');
+    alert('numero: ${de}');
+}
